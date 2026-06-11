d09776a1282365480f278d1ed6d28d3edd618301
Radhika Rungta <radhika.rungta@flamapp.com>
2026-06-11 17:40:36 +0530

    Update network_gui.py

--- gaussian_renderer/network_gui.py
+++ gaussian_renderer/network_gui.py
@@ -5,7 +5,7 @@
 #
 # This software is free for non-commercial, research and evaluation use 
 # under the terms of the LICENSE.md file.
-#
+# 
 # For inquiries contact  george.drettakis@inria.fr
 #
 
